d24a9a89c0994ca803cf96c1e2f441fbeb5968ac
Rupinder <rupinder.singh@finmechanics.com>
2023-12-29 13:33:56 +0530

    @basic setup

---
+++ App.js
@@ -0,0 +1,39 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+export const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkNB-4DhaTiN4wNlwIbOrJC-T2yKlpta0qPKXgJa4xxpZlLTZE" />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+
+export const Body = () => {
+  return (
+    <div>Body</div>
+  )
+}
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
